6b82ce87b1dc8c30d6604d5f40c4c09921554bc0
Maksym <praniknumberone@gmail.com>
2025-12-30 03:33:53 +0100

    DoneHomework

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+//сподіваюся,що я правильно зрозумів завдання
+let airTemperatureCelsius = 24; //  let  тому,що температура постійно змінюється
+let bookTitle = undefined; //  не знаю,як пояснити,чому let,але let
+const Length = 3; //  const,тому,що розміри не змінюються
+const Width = 4; //  const,тому,що розміри не змінюються
+const Area = Length * Width; //  const,тому,що площа не змінюється
+const isTriangle = false; //  const,тому,що це булеве значення не змінюється
+const element = null; //  const,тому,що елемент не змінюється,null - це спеціальне значення,яке вказує на відсутність об'єкта,але ми знаємо що він є
